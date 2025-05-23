8e98ae26608f3366b7d0abdcaf3e5ed42e86f5bc
Douglas Rodrigues <douglasrp@id.uff.br>
2025-05-23 14:53:32 -0300

    Apenas teste de aprendizado

---
+++ comandosPy.sh
@@ -0,0 +1,24 @@
+#clonar o repositorio python
+git clone https://github.com/CienciaDeDadosUFF/python.github.io.git
+
+#verificar status dos arquivos
+git status
+#versao resumida:??=nao rastr,A=Stagged,M=Modificado
+git status -s
+
+#rastrear novos arquivos
+git add <nome do arquivo>
+
+#adicionar todos arq. mod. para staged
+git add .
+
+#Realizar o commit comentado
+git commit -m "Comentários aqui"
+
+#enviar commit para github
+git push
+
+#atualizar os arquivos locais
+git pull 
+
+
